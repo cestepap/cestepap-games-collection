9e0e96fc6a9ec76d9b9b6da112b4fb89b3e89ad7
Carlos Estepa <carlos.estepa84@gmail.com>
2022-10-16 10:37:26 +0200

    b7

--- server/build/index.js
+++ server/build/index.js
@@ -1,39 +1,43 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const morgan_1 = __importDefault(require("morgan"));
-const cors_1 = __importDefault(require("cors"));
-const config_1 = require("./config");
-const gamesRoutes_1 = __importDefault(require("./routes/gamesRoutes"));
+'use strict';
+var __importDefault =
+	(this && this.__importDefault) ||
+	function (mod) {
+		return mod && mod.__esModule ? mod : { default: mod };
+	};
+Object.defineProperty(exports, '__esModule', { value: true });
+const express_1 = __importDefault(require('express'));
+const morgan_1 = __importDefault(require('morgan'));
+const cors_1 = __importDefault(require('cors'));
+const config_1 = require('./config');
+const gamesRoutes_1 = __importDefault(require('./routes/gamesRoutes'));
 class Server {
-    constructor() {
-        this.app = (0, express_1.default)();
-        this.config();
-        this.routes();
-    }
-    config() {
-        // this.app.set('port', process.env.PORT || 3000);
-        this.app.use((0, morgan_1.default)('dev'));
-        this.app.use((0, cors_1.default)());
-        this.app.use(express_1.default.json());
-        // por si queremos enviar desde formulario html
-        this.app.use(express_1.default.urlencoded({ extended: false }));
-    }
-    routes() {
-        this.app.use('/api/games', gamesRoutes_1.default);
-        this.app.use((req, res, next) => {
-            res.status(404).json({
-                message: 'ruta not found',
-            });
-        });
-    }
-    start() {
-        this.app.listen(config_1.PORT);
-        console.log('Server runninggg on port', config_1.PORT);
-    }
+	constructor() {
+		this.app = (0, express_1.default)();
+		this.config();
+		this.routes();
+	}
+	config() {
+		// this.app.set('port', process.env.PORT || 3000);
+		this.app.use((0, morgan_1.default)('dev'));
+		this.app.use((0, cors_1.default)());
+		this.app.use(express_1.default.json());
+		// por si queremos enviar desde formulario html
+		this.app.use(express_1.default.urlencoded({ extended: false }));
+	}
+	routes() {
+		this.app.use('/api/games', gamesRoutes_1.default);
+		this.app.use((req, res, next) => {
+			res.status(404).json({
+				message: 'ruta not found',
+			});
+		});
+	}
+	start() {
+		this.app.listen(config_1.PORT);
+		console.log('Server runninggg on port', config_1.PORT);
+		console.log(__dirname);
+		console.log(process.cwd());
+	}
 }
 const server = new Server();
 server.start();
